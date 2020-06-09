ef521aa7dd287a910f58c4a6e2906ab0008f5130
Wesley <wesaboo@gmail.com>
2020-06-08 17:38:04 -0700

    start

---
+++ index.js
@@ -0,0 +1,15 @@
+// Grabbing required modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+
+// Global variable to store input
+let inputData = [];
+
+// Inquirer to ask questions
+inquirer.prompt([
+
+]).then(function(response) {
+    console.log(response);
+});
+
