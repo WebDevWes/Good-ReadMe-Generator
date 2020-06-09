a26168a991b51e9523fa47e38e7f89fe5cce34d9
Wesley <wesaboo@gmail.com>
2020-06-09 14:54:24 -0700

    adds all questions for inquirer, add gitignore

--- index.js
+++ index.js
@@ -9,12 +9,54 @@ inquirer.prompt([
         message:"Please enter your Github username",
         name: "username",
     },
+    {
+        type:"input",
+        message:"Please enter a title for your Project/Repo",
+        name: "title",
+    },
+    {
+        type:"input",
+        message:"Please enter a brief description of your Project/Repo",
+        name: "description",
+    },
+    {
+        type:"input",
+        message:"Please enter a table of contents if you have one",
+        name: "tableContents",
+    },
+    {
+        type:"input",
+        message:"Please enter any installation instructions if neccessary",
+        name: "installation",
+    },
+    {
+        type:"input",
+        message:"Please describe how to use this specific application/repo, add screenshots if neccessary",
+        name: "usage",
+    },
+    {
+        type:"list",
+        choices:["ISC","GPL","MIT","APACHE","BSD"],
+        message:"Please select the license for your Project/Repo",
+        name: "license",
+    },
+    {
+        type:"input",
+        message:"Please enter the contributor's name(s) for this Project/Repo",
+        name: "contributor",
+    },
+    {
+        type:"input",
+        message:"If you have a test code for your Project/Repo, please enter it here",
+        name: "test",
+    },
 ]).then(function(response) { 
-    let readMe = `
-    # github:${userData.github}
-    (https://img.shields.io/badge/license-${userData.gLicense}-blue.svg)
-    `
-    
+    let readMe = `https://img.shields.io/badge/license-${response.license}-green.svg
+    Github Username: ${response.username}
+    Project/Repo Title: ${response.title}
+    Description: ${response.description}
+    `;
+    // Write to file README.md
     fs.writeFile("README.md", JSON.stringify(readMe, null, "\n"), function(err) {
         if (err) {
             console.log('Oops, there was an error', err);
