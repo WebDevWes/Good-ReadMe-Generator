ed3b50c70672dda160808d55b42ed8735b6cbddb
Wesley <wesaboo@gmail.com>
2020-06-09 03:29:36 -0700

    adds writefile and inquirer prompt

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 // Grabbing required modules
 const fs = require("fs");
 const inquirer = require("inquirer");
-const axios = require("axios");
-
-// Global variable to store input
-let inputData = [];
 
 // Inquirer to ask questions
 inquirer.prompt([
-
-]).then(function(response) {
-    console.log(response);
+    {
+        type:"input",
+        message:"Please enter your Github username",
+        name: "username",
+    },
+]).then(function(response) { 
+    let readMe = `
+    # github:${userData.github}
+    ## title:${userData.pTittle}
+    (https://img.shields.io/badge/license-${userData.gLicense}-blue.svg)
+    follows:${githubData.data.follows}
+    
+        `
+    
+    fs.writeFile("README.md", JSON.stringify(readMe, null, "\n"), function(err) {
+        if (err) {
+            console.log('Oops, there was an error', err);
+        }
+        console.log('Your file was written!  Success!');
+    });
 });
 
